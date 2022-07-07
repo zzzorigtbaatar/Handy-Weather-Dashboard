9d52610678f27335084ef6e4db55980881cf5b15
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-06 20:58:54 -0700

    add display city name and weather icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ var searchInput = $('#search-input');
 // var searchFormEl = $('#search-form');
 var searchBtnEl = $('#search-btn');
 
+var cityTitleEl = $('#current-city-title');
+
+var cityName = "";
+var cityState = "";
+var cityCountry = "";
+var cityLat = 0;
+var cityLon = 0;
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -28,11 +35,37 @@ function displayLocation(searchQueryURL) {
         })
         .then(function (data) {
             console.log(data);
-            
+
+            cityName = data[0].name;
+            cityState = data[0].state;
+            cityCountry = data[0].country;
+            cityLat = data[0].lat;
+            cityLon = data[0].lon;
+
+            var queryWeatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&APPID=4cf13e749504309d50ec21fe5fae86a6';
+
+            getWeatherIcon(queryWeatherURL);
+
+
+
         })
         .catch(function (error) {
             console.error(error);
         });
+}
+
+function getWeatherIcon(queryWeatherURL) {
+    fetch(queryWeatherURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            cityTitleEl.html(cityName + ', ' + cityState + ', ' + cityCountry + " (" + moment().format("MM/DD/YYYY") + ") <img id='currentIcon' src='https://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png' />");
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
 }
 // searchFormEl.addEventListener('submit', handleSubmit);
 searchBtnEl.click(handleSubmit);
